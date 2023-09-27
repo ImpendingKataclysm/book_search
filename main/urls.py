39b20b9779570f5038f1fd2b3bdb5faa268e632e
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-27 15:53:05 -0700

    Add a base template with a logo and some styles, and a home page extending from it.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'book_search'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.SearchPage.as_view(), name='search'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
