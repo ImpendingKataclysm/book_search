dde293b6e9b4c9f04c9ecfdda2203d0b97ac5919
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-27 15:59:12 -0700

    Create a form to search for a book by title on the home page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from . import forms
 
 
-class SearchPage(generic.TemplateView):
+class SearchPage(generic.FormView):
     template_name = 'index.html'
+    form_class = forms.TitleSearchForm
+    success_url = '/'
