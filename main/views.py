667c82a3373b6c94c912779468a45a2de8e491bf
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-28 13:32:48 -0700

    Send the book title entered by the user to the OpenLibrary API as a search query and display the titles, authors, cover images and publication dates in the Results page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render, redirect
 from django.views import generic, View
 from django.urls import reverse
@@ -10,14 +11,23 @@ class SearchPage(generic.FormView):
     success_url = '/results/'
 
     def form_valid(self, form):
-        return super(SearchPage, self).form_valid()
+        search_query = form.cleaned_data['book_title']
+        return super(SearchPage, self).form_valid(form)
 
 
 class ResultsPage(generic.TemplateView):
     template_name = 'results.html'
+    search_api_url = 'https://openlibrary.org/search.json'
 
     def get_context_data(self, **kwargs):
         context = super(ResultsPage, self).get_context_data()
-        context['search_query'] = self.request.GET['book_title']
+        search_query = self.request.GET.get('book_title')
+
+        if search_query:
+            response = requests.get(self.search_api_url, params={'title': search_query})
+            books = response.json()['docs']
+            titles = [book['title'] for book in books]
+            context['search_query'] = search_query
+            context['books'] = books
 
         return context
