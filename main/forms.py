2413fe6cdde75f8d84c3a0587f4b46528a92a408
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-11-15 15:20:40 -0800

    Improved visual display of the search form

--- main/forms.py
+++ main/forms.py
@@ -7,15 +7,24 @@ class SearchForm(forms.Form):
     book_title = forms.CharField(
         max_length=MAX_FIELD_LEN,
         required=False,
-        widget=forms.TextInput(attrs={'placeholder': 'Title'})
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Title',
+            'class': 'form-control'
+        })
     )
     author_name = forms.CharField(
         max_length=MAX_FIELD_LEN,
         required=False,
-        widget=forms.TextInput(attrs={'placeholder': 'Author'})
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Author',
+            'class': 'form-control'
+        })
     )
     subject_keyword = forms.CharField(
         max_length=MAX_FIELD_LEN,
         required=False,
-        widget=forms.TextInput(attrs={'placeholder': 'Subject'})
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Subject',
+            'class': 'form-control'
+        })
     )
